29fd577cac1322984d8047649a669c96aef05c79
Dell <muskan.mittal_cs18@gla.ac.in>
2021-11-27 11:56:50 +0530

    first commit using vs code

---
+++ input.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class One{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter first name");
+        String first = sc.next();
+        System.out.println("second name : ");
+        String second = sc.next();
+
+    System.out.println(first+ " "+second);
+    }
+}
